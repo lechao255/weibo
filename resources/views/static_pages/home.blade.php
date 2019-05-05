e2da46f1a78c2ca0e37458e3708680236985d988
Liuxq <605866812@qq.com>
2019-05-05 03:19:18 +0000

    初始化基础样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.default')
 
 @section('content')
-	<h1>主页</h1>
+	<div class="jumbotron">
+		<h1>Hello Laravel</h1>
+		<p class="lead">
+			熟知 <a href="https://learnku.com/courses/laravel-essential-training" target="_blank">Laravel框架</a> 的开始。
+		</p>
+
+		<p>
+			一切，将从这里开始。
+		</p>
+
+		<p>
+			<a href="#" class="btn btn-lg btn-success" role='button'>现在注册</a>
+		</p>
+	</div>
 @stop
