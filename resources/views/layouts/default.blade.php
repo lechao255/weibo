1651486238044c3991d850ba0a14dc96b0fcf143
Liuxq <605866812@qq.com>
2019-05-05 03:58:34 +0000

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,6 +11,7 @@
 			<ul class="navbar-nav justify-content-end">
 				<li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
 				<li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+				<li class="nav-item"><a href="#" class="nav-link">测试</a></li>
 			</ul>
 		</div>
 	</nav>
