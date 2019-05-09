33a8b32b714e749bf4413957e76cd0f087a5a3cd
Liuxq <605866812@qq.com>
2019-05-09 09:55:58 +0000

    Update database configuration

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,12 +13,15 @@ class UserController extends Controller
     	return view('users.create');
     }
 
-    //
+    // 展示个人详情
     public function show(User $user){
     	return view('users.show', compact('user'));
+    	// 如上的compact用法可以这样理解
+    	// 寻找以compact括号内的参数（user）作为变量名的变量（$user），如果存在，则以此参数（user）作为数组键，此变量（$user）的值作为数组值
+    	// return view('users.show', ['user' => $user]);
     }
 
-    //
+    // 保存用户数据
     public function store(Request $request){
     	$this->validate($request, ['name' => 'required|max:50', 'email' => 'required|email|unique:users|max:255', 'password' => 'required|confirmed|min:6']);
     	
@@ -28,6 +31,8 @@ class UserController extends Controller
     		'password' => bcrypt($request->password),
     	]);
 
+    	// 由于http协议是无状态的，所以Laravel提供了一种用于临时保存用户数据的方法-会话（Session）
+    	// flash方法保存的数据只会保留到下个HTTP请求到来之前，然后就会被删除，闪存数据主要用于短期的状态消息
     	session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
 
     	return redirect()->route('users.show', [$user]);
--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$db_config = get_db_config();
+
 return [
 
     /*
@@ -13,7 +15,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
@@ -54,15 +57,20 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),]) : [],
         ],
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'database' => env('DB_DATABASE', 'forge'),
+            'database' => $db_config['database'],
+            //'username' => env('DB_USERNAME', 'forge'),
+            'username' => $db_config['username'],
+            //'password' => env('DB_PASSWORD', ''),
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
