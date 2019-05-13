5c0b5fdf6b577ea47a9cdc908b669a3aeba19c50
Liuxq <605866812@qq.com>
2019-05-13 04:39:11 +0000

    更改用户资料

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,4 +46,26 @@ class UserController extends Controller
 
     	return redirect()->route('users.show', [$user]);
     }
+
+    // 编辑用户信息页面
+    public function edit(User $user){
+    	return view('users.edit', compact('user'));
+    }
+
+    // 保存用户编辑信息
+    public function update(User $user, Request $request){
+    	$this->validate($request, ['name' => 'required|max:50', 'password' => 'nullable|confirmed|min:6']);
+
+    	$data = [];
+    	$data ['name'] = $request->name;
+    	if ($request->password) {
+    		$data ['password'] = bcrypt($request->password);
+    	}
+
+    	$user->update($data);
+
+    	session()->flash('success', '个人资料更新成功！');
+
+    	return redirect()->route('users.show', $user->id);
+    }
 }
