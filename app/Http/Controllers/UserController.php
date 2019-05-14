34557eaa089487f0e6c7438aa2e7727f8bd515a4
Liuxq <605866812@qq.com>
2019-05-14 04:21:35 +0000

    管理员可删除用户

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,4 +82,12 @@ class UserController extends Controller
 
     	return redirect()->route('users.show', $user->id);
     }
+
+    // 管理员删除用户
+    public function destroy(User $user){
+    	$this->authorize('destroy', $user);
+    	$user->delete();
+    	session()->flash('success', '成功删除用户！');
+    	return back();
+    }
 }
--- resources/views/users/_user.blade.php
+++ resources/views/users/_user.blade.php
@@ -1,4 +1,11 @@
 <div class="list-group-item">
 	<img src="{{ $user->gravatar() }}" class="mr-3" alt="{{ $user->name }}" width="32">
 	<a href="{{ route('users.show', $user) }}" target="_blank">{{ $user->name }}</a>
+	@can('destroy', $user)
+		<form action="{{ route('users.destroy', $user->id) }}" method="post" class="float-right">
+			{{ csrf_field() }}
+			{{ method_field('DELETE') }}
+			<button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+		</form>
+	@endcan
 </div>
