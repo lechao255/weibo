5491cafa1e62ded03342d1c9fae77d47c0df2e23
Liuxq <605866812@qq.com>
2019-05-15 03:45:04 +0000

    邮件发送至邮箱

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -100,13 +100,13 @@ class UserController extends Controller
     public function sendEmailConfirmationTo($user){
     	$view = 'emails.confirm';
     	$data = compact('user');
-    	$from = 'admin@qq.com';
-    	$name = 'admin';
+    	//$from = 'admin@qq.com';
+    	//$name = 'admin';
     	$to = $user->email;
     	$subject = "感谢注册 Weibo 应用！请确认您的邮箱。";
 
-    	Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-    		$message->from($from, $name)->to($to)->subject($subject);
+    	Mail::send($view, $data, function ($message) use ($to, $subject) {
+    		$message->to($to)->subject($subject);
     	});
     }
 
