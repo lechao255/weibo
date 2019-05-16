e73bf6e7ca18c6678f307b5474e54595e2e5fd6b
Liuxq <605866812@qq.com>
2019-05-16 02:31:54 +0000

    发布微博

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.default')
 
 @section('content')
+	@if (Auth::check())
+	<div class="row">
+		<div class="col-md-8">
+			<section class="status_form">@include('shared._status_form')</section>
+		</div>
+		<aside class="col-md-4">
+			<section class="user_info">
+				@include('shared._user_info', ['user' => Auth::user()])
+			</section>
+		</aside>
+	</div>
+	@else
 	<div class="jumbotron">
 		<h1>Hello Laravel</h1>
 		<p class="lead">
@@ -15,4 +27,5 @@
 			<a href="{{ route('signup') }}" class="btn btn-lg btn-success" role='button'>现在注册</a>
 		</p>
 	</div>
+	@endif
 @stop
